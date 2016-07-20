0c472581a57ec955a0e8ee4d5a42c44124ab36e6
Geunho Khim <gkhim@ebay.com>
2016-07-20 16:36:42 +0900

    [split-string] 문자열 Split() 테스트 추가

---
+++ split-string/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 어셈블리의 일반 정보는 다음 특성 집합을 통해 제어됩니다.
+// 어셈블리와 관련된 정보를 수정하려면
+// 이 특성 값을 변경하십시오.
+[assembly: AssemblyTitle("split-string-t")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("eBay")]
+[assembly: AssemblyProduct("split-string-t")]
+[assembly: AssemblyCopyright("Copyright © eBay 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible을 false로 설정하면 이 어셈블리의 형식이 COM 구성 요소에 
+// 노출되지 않습니다. COM에서 이 어셈블리의 형식에 액세스하려면 
+// 해당 형식에 대해 ComVisible 특성을 true로 설정하십시오.
+[assembly: ComVisible(false)]
+
+// 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
+[assembly: Guid("98d6fb84-cf50-4f61-adb3-a9b60387ae93")]
+
+// 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
+//
+//      주 버전
+//      부 버전 
+//      빌드 번호
+//      수정 버전
+//
+// 모든 값을 지정하거나 아래와 같이 빌드 번호 및 수정 버전이 자동으로 
+// 지정되도록 할 수 있습니다.
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ split-string/Split/SplitTest.cs
@@ -0,0 +1,84 @@
+﻿namespace Geunho.Net.Test
+{
+    using System;
+    using System.Diagnostics;
+    using System.Text.RegularExpressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class SplitTest
+    {
+        #region Constants
+        const string _stringToSplit = "apple:: banana::mango ";
+        const string _splitToken = "::";
+        const int _numberOfLoops = 10000;
+
+        static Regex _preCompiledRegex = new Regex(_splitToken, RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+        #endregion
+
+        [TestMethod]
+        public void RegexSplitShouldSlowerThanStringSplit()
+        {
+            TimeSpan _elapsedUsingCompiledRegex;
+            TimeSpan _elapsedUsingRegex;
+            TimeSpan _elapsedUsingString;
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            #region pre-compiled Regex loop
+
+            for (int i = 0; i < _numberOfLoops; i++)
+            {
+                _preCompiledRegex.Split(_stringToSplit);
+            }
+
+            stopwatch.Stop();
+            _elapsedUsingCompiledRegex = stopwatch.Elapsed;
+
+            Console.WriteLine("Time elapsed (pre-compiled Regex): {0} ms", _elapsedUsingCompiledRegex.TotalMilliseconds);
+
+            #endregion
+
+            stopwatch.Restart();
+
+            #region Regex loop
+
+            for (int i = 0; i < _numberOfLoops; i++)
+            {
+                Regex.Split(_stringToSplit, _splitToken, RegexOptions.IgnorePatternWhitespace);
+            }
+
+            stopwatch.Stop();
+            _elapsedUsingRegex = stopwatch.Elapsed;
+
+            Console.WriteLine("Time elapsed (Regex): {0} ms", _elapsedUsingRegex.TotalMilliseconds);
+
+            #endregion
+
+            stopwatch.Restart();
+
+            #region String loop
+
+            string[] splitTokens = new string[] { _splitToken };
+                        
+            for (int i = 0; i < _numberOfLoops; i++)
+            {
+                _stringToSplit.Split(splitTokens, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            stopwatch.Stop();
+            _elapsedUsingString = stopwatch.Elapsed;           
+
+            Console.WriteLine("Time elapsed (String): {0} ms", _elapsedUsingString.TotalMilliseconds);
+
+            #endregion
+
+            stopwatch.Reset();            
+
+            Assert.IsTrue(_elapsedUsingRegex.Ticks > _elapsedUsingString.Ticks, "Is Regex split slower than String split?");
+            Assert.IsTrue(_elapsedUsingRegex.Ticks > _elapsedUsingCompiledRegex.Ticks, "Is Regex split slower than pre-compiled Regex split?");
+        }
+    }
+}
